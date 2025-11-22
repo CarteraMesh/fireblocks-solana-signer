d868cf19d6da5c52b539f34b234f3039a2e4f1dd
Douglas Chimento <dchimento@gmail.com>
2025-11-22 15:25:27 +0000

    fix: test once (#58)

--- tests/tokio.rs
+++ tests/tokio.rs
@@ -29,30 +29,6 @@ async fn test_tokio() -> anyhow::Result<()> {
     // will use the tokio version of sign_message
     let mut tx = Transaction::new_unsigned(message);
     tx.try_sign(&[&signer], hash)?;
-
-    let signature = tx.get_signature();
-    tracing::info!("Transaction signature: {:?}", signature);
-    rpc.send_and_confirm_transaction(&tx).await?;
-    Ok(())
-}
-
-#[tokio::test(flavor = "multi_thread", worker_threads = 4)]
-async fn test_tokio_single() -> anyhow::Result<()> {
-    setup();
-    let (signer, _) = signer()?;
-    let rpc = solana_rpc_client::nonblocking::rpc_client::RpcClient::new(
-        std::env::var("RPC_URL")
-            .ok()
-            .unwrap_or("https://rpc.ankr.com/solana_devnet".to_string()),
-    );
-    let hash = rpc.get_latest_blockhash().await?;
-    let message = Message::new_with_blockhash(
-        &[memo("fireblocks signer tokio", &signer.pk)],
-        Some(&signer.pk),
-        &hash,
-    );
-    let message = VersionedMessage::Legacy(message);
-    let tx = VersionedTransaction::try_new(message, &[&signer])?;
     let signature = tx.get_signature();
     tracing::info!("Transaction signature: {:?}", signature);
     rpc.send_and_confirm_transaction(&tx).await?;
